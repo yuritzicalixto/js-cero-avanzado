f0336ca513466222de953dbf928a79d85d2b653b
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-09-04 18:26:44 -0600

    Promp, confirm and alert

---
+++ js/alerts.js
@@ -0,0 +1,8 @@
+// alert('Hola Mundo');
+
+// prompt('¿Cuál es tu nombre? ', )
+
+// let nombre = prompt('¿Cuál es tu nombre? ');
+
+const selecion = confirm('¿Seguro de borrar eso? ');
+console.log(selecion);
---
+++ js/app.js
@@ -0,0 +1,13 @@
+// alert('Hola desde app.js');
+
+console.log( console.log('Hola Mundo') );
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+console.log('%c Mis variables', 'color:blue; font-weight:bold');
